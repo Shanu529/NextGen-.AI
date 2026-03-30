3e6b0f365757750935b0a3407e339b3d5c3c2f3a
Shanu Chhetri <shnau.chhetri529@gmail.com>
2026-03-30 12:26:42 +0530

    ...

--- server/server.js
+++ server/server.js
@@ -1,21 +1,13 @@
-
 import express from 'express';
 import cors from 'cors';
-import axios from 'axios';
-import connectDB from './config/mongodb.js'
+import connectDB from './config/mongodb.js';
 import dotenv from "dotenv";
 import userRouter from './Router/userRouter.js';
-dotenv.config();
 import imageRouter from './Router/imageRouter.js';
-// import { useEffect } from 'react';
 
+dotenv.config();
 
 const app = express();
-dotenv.config();
-
-
-// const PORT = process.env.PORT || 4005  
-
 
 app.use(cors({
   origin: "https://next-gen-ai-gold.vercel.app",
@@ -23,33 +15,20 @@ app.use(cors({
   credentials: true
 }));
 
-app.options("*", cors()); // 
+app.options("*", cors());
 
 app.use(express.json());
-connectDB()
 
-app.use((req, res, next) => {
-  console.log("----- REQUEST HEADERS -----");
-  console.log(req.headers);
-
-  console.log("----- REQUEST BODY -----");
-  console.log(req.body);
-
-  next(); // Continue to next route
+app.use(async (req, res, next) => {
+  await connectDB();
+  next();
 });
 
 app.use('/api/user', userRouter);
 app.use('/api/image', imageRouter);
 
 app.get("/", (req, res) => {
-  res.send("working")
-  console.log("run")
+  res.send("working");
 });
 
 export default app;
-
-// app.listen(PORT, () => {
-//   console.log(`server running on this port ${PORT}`)
-
-// });
-
